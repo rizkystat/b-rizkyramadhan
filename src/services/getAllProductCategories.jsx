ca73e257407db8b0128408906063ea77ad33a28a
unknown <riskyramadan1204@gmail.com>
2024-11-18 11:15:32 +0700

    commit

---
+++ src/services/getAllProductCategories.jsx
@@ -0,0 +1,15 @@
+const getAllProductCategories = ()  => {
+  return [   {
+    id: 'FMEN',
+    slug: 'men-shirt',
+    name: `Men's Shirt`
+  },
+  {
+    id: 'FWMEN',
+    slug: 'women-shirt',
+    name: `Women's Shirt`
+  },
+  ]
+}
+
+export default getAllProductCategories
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,60 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'FMEN-001',
+      slug: 'men-shirt',
+      name: "Baju model 1",
+      category: `Men's Shirt`,
+      categorySlug: 'men-shirt',
+      price: 1_549_000,
+      stock: 100,
+      imageUrl: '/assets/images/bajuL1.jpg',
+      description: `The radiance fresh spin on what you know best: crisp leather, bold colours and the perfect amount of flash to make you shine.`
+    },
+    {
+      id: 'FMEN-002',
+      slug: 'men-shirt',
+      name: "baju model 2",
+      category: `Men's Shirt`,
+      categorySlug: 'men-shirt',
+      price: 1_909_000,
+      stock: 55,
+      imageUrl: `/assets/images/BAJUL2.jpg`,
+      description: `Comfortable, bold details for style that tracks whether you're on court or on the go.`
+    },
+    {
+      id: 'FMEN-003',
+      slug: 'men-shirt',
+      name: "Polo shirt",
+      category: `men's Shirt`,
+      categorySlug: 'men-shirt',
+      price: 1_909_000,
+      stock: 90,
+      imageUrl: `/assets/images/POLO.jpg`,
+      description: `Comfortable, durable and timeless—it's a warm suede upper with neutral accents for style that tracks whether you're on court or on the go.`
+    },
+
+    {
+      id: 'FWOMEN-004',
+      slug: 'women-shirt',
+      name: "women clothe",
+      category: `Women's Shirt`,
+      categorySlug: 'women-shirt',
+      price: 1_900_000,
+      stock: 90,
+      imageUrl: `/assets/images/CW.jpg`,
+      description: `Daster wanita ini terbuat dari bahan katun yang halus dan lembut, sehingga nyaman dipakai. Daster ini memiliki desain yang simpel dan elegan, cocok sekali untuk dipakai dalam berbagai acara. Tersedia dalam berbagai warna yang menarik, sehingga kamu dapat menyesuaikan dengan selera kamu.`
+    },
+    {
+      id: 'FWOMEN-005',
+      slug: 'women-shirt',
+      name: "women clothe 2",
+      category: `Women's Shirt`,
+      categorySlug: 'women-shirt',
+      price: 1_900_000,
+      stock: 90,
+      imageUrl: `/assets/images/CW2.jpg`,
+      description: `Daster wanita ini terbuat dari bahan katun yang halus dan lembut, sehingga nyaman dipakai. Daster ini memiliki desain yang simpel dan elegan, cocok sekali untuk dipakai dalam berbagai acara. Tersedia dalam berbagai warna yang menarik, sehingga kamu dapat menyesuaikan dengan selera kamu.`
+    },
+  ]
+}
